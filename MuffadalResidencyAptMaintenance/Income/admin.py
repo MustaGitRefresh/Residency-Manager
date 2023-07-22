02f3c5242d723f38a5f7ad91283ccc7043fabc34
MustaGitRefresh <kabcoco.2gitdom.3@gmail.com>
2023-07-22 23:36:29 +0530

    All setup is done now just have to find the use of Record class and also find the way to ask flat number from user in three digit format not consecutive format.

--- MuffadalResidencyAptMaintenance/Income/admin.py
+++ MuffadalResidencyAptMaintenance/Income/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import IncomeModel
+
 
 # Register your models here.
+@admin.register(IncomeModel)
+class IncomeAdmin(admin.ModelAdmin):
+    class Meta:
+        list_display = ['flat_no', 'date', "Amount", "maintenance_giver", "number"]
+        fields = "__all__"
--- MuffadalResidencyAptMaintenance/Income/models.py
+++ MuffadalResidencyAptMaintenance/Income/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class IncomeModel(models.Model):
+    flat_no = models.IntegerField(primary_key=True, null=False)
+    date = models.DateField()
+    Amount = models.IntegerField(default="Not paid")
+    maintenance_giver = models.CharField(max_length=30)
+
